67db7871bd7525688fd94ab20004d18812b344f4
Marti D <me@martidolce.com>
2021-04-20 18:59:31 -0400

    Setup Azure App Resource
    https://docs.microsoft.com/en-us/graph/tutorials/ruby

---
+++ config/oauth_environment_variables.rb
@@ -0,0 +1,3 @@
+ENV['AZURE_APP_ID'] = '94a3c523-cd01-47f8-9f2f-eb12b696fbb9'
+ENV['AZURE_APP_SECRET'] = 'lyapJDZ206+xcxLWSB71!#;'
+ENV['AZURE_SCOPES'] = 'openid profile email offline_access user.read calendars.read'
