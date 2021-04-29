b0b7059c62d587e79af5a7eb049f3fe57e111187
Marti D <me@martidolce.com>
2021-04-29 06:49:54 -0400

    Start User MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  # <!--============================= USERS   =============================-->
+  # The application provides a way for HOA's to manage their properties, tenants and owners
+  # Start with who will have access.  Users are defaulted to tenants, owners or admins.
+
+  # Build the MVC rails g resource Users fname, lname, email with unique, password
+  # bundle exec rails g resource User fname:string lname:string email:string
+
   resources :users
-  resources :widgets
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # <!--============================= WELCOME =============================-->
+  # Default Home page
 
   root 'welcome#index'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
