17e2e3fd31832ea9040c9a11f473d2a6c2495450
Marti D <me@martidolce.com>
2021-04-29 14:53:45 -0400

    Static Page Controller, Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
 
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
 
   # You can have the root of your site routed with "root"
 
   root 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
